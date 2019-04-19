ca9cb2ad2544db0157d15269e494c9d67d74cbef
Stephanie <46686745+choisteph@users.noreply.github.com>
2019-04-18 22:52:16 -0400

    z indexed

--- js/index.js
+++ js/index.js
@@ -80,17 +80,15 @@ function checkForm(){
 
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
-let navbar = document.querySelector("#navbar");
+var navbar = document.querySelector("#navbar");
 
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
-}
+};
